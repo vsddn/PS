83596c6b08dc59c7f6aeb358756a0fa0666e4c99
vijay singh <vs720721@gmail.com>
2020-04-09 22:23:18 +0530

    First Commit

---
+++ java-project/src/main/java/com/sapient/week1/App.java
@@ -0,0 +1,21 @@
+package com.sapient.week1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
+
+class CheckEven{
+    public Boolean check(int n){
+        if(n%2==0) return true;
+        else return false;
+    }
+}
